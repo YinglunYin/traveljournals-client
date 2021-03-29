1a2e462d13de8d6133c0e9d75f8c418622665242
Yinglun Yin <adsl7377@163.com>
2021-03-30 04:41:28 +0800

    wiki

--- src/services/map-services.js
+++ src/services/map-services.js
@@ -1,7 +1,7 @@
 const KEY = "AIzaSyAhvVlcZCZpedYfhf89Mgn2_bC6a2pecCQ"
 
 const findPlacesListByText = (placeName) =>
-    fetch(`https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/textsearch/json?query=${placeName}&key=${KEY}`)
+    fetch(`https://mycorsforfinal.herokuapp.com//https://maps.googleapis.com/maps/api/place/textsearch/json?query=${placeName}&key=${KEY}`)
         .then(response => response.json())
 
 
